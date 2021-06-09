3abe01eb5c6800622cad39f24c0407b508257bde
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-06-09 14:06:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-// classes
-import { Invoice } from './classes/Invoice.js';
-const invOne = new Invoice('chris', 'work on the Chris website', 250);
-const invTwo = new Invoice('lisa', 'work on the Lisa website', 300);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach(inv => {
-    console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
-});
+const me = {
+    name: 'shaun',
+    //age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent ', amount);
+        return amount;
+    },
+};
+console.log(me);
+me.speak('hello, world');
+const greetPerson = (person) => {
+    console.log('hello ', person.name);
+};
+greetPerson(me);
+//greetPerson({name: 'shaun'});
 const form = document.querySelector('.new-item-form');
 console.log(form.children);
 // inputs
