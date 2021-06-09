6012c643f0f5cfb362470ef30488e84168b8a3c3
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-06-09 13:12:11 -0500

    Classes

--- app.ts
+++ app.ts
@@ -1,26 +1,31 @@
-const anchor = document.querySelector('a')!;
-if(anchor) {
-    console.log(anchor.href);
-}
-console.log(anchor.href);
-
-//const form = document.querySelector('form')!;
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
+"use strict";
+// classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes \u00A3" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('Chris', 'work on the Chris website', 250);
+var invTwo = new Invoice('Lisa', 'work on the Lisa website', 300);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+// invoices.push({ name: 'shaun' });
+console.log(invoices);
+var form = document.querySelector('.new-item-form');
 console.log(form.children);
-
 // inputs
-const type = document.querySelector('#type') as HTMLInputElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
-
-form.addEventListener('submit', (e: Event) => {
+var type = document.querySelector('#type');
+var tofrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
     e.preventDefault();
-
-    console.log(
-        type.value,
-        tofrom.value,
-        details.value,
-        amount.valueAsNumber
-    );
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
