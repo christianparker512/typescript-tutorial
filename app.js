902ec51813c3e8f39da88418bc2b77bce09b97c0
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-06-09 13:13:00 -0500

    classes

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
+
 "use strict";
-var anchor = document.querySelector('a');
-if (anchor) {
-    console.log(anchor.href);
-}
-console.log(anchor.href);
-//const form = document.querySelector('form')!;
+// classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes \u00A3" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('Chris', 'work on the Chris website', 250);
+var invTwo = new Invoice('Lisa', 'work on the Lisa website', 300);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+// invoices.push({ name: 'shaun' });
+console.log(invoices);
 var form = document.querySelector('.new-item-form');
 console.log(form.children);
 // inputs
