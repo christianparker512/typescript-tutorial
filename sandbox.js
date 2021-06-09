7cd2ef30122aa77f1fe48e6b7c8cf9dbf09b949b
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-06-09 10:36:57 -0500

    function basics - 8

--- sandbox.js
+++ sandbox.js
@@ -1,46 +1,16 @@
-// //const character = 'Christian';
-// let character = 'Christian';
-// let age = 50;
-// let isProgrammer = false;
-//
-// //character = 20;
-// character = 'parker';
-//
-// age = 30;
-//
-// isProgrammer = true;
-//
-// const circ = (diameter:number) => {
-//     return diameter * Math.PI
-// }
-//
-// console.log(circ(7.5));
-// console.log(character);
-//
-// const inputs = document.querySelectorAll('input');
-//
-// inputs.forEach(input => {
-//     console.log(input);
-// });
-
-let names = ['chris', 'parker', 'lisa'];
-
-names.push('abby');
-
-let numbers = [10,20,30,40];
-numbers.push(25);
-
-let mixed = ['baxter', 3, 'quincy', 8, 9];
-mixed.push('zeke');
-mixed.push(10);
-mixed[0] = 3;
-// the below will not compile due to them not being strings
-// names.push(3);
-// names.push[0];
-
-//Cant add new properties or change only a few
-let parkers = {
-    name: 'chris',
-    color: 'blue',
-    age: 50
-}
+"use strict";
+var greet;
+//greet = 'hello';
+greet = function () {
+    console.log('hello,again');
+};
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, 20);
+var minus = function (a, b) {
+    return a + b;
+};
+// let result = minus(10,7);
