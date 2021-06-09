613aee4b25da1c45248ae442f340972e95fc2588
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-06-09 15:31:43 -0500

    final session of typescript

--- app.ts
+++ app.ts
@@ -1,46 +1,46 @@
 import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
 import { ListTemplate } from './classes/ListTemplate.js';
-const form = document.querySelector('.new-item-form');
-console.log(form.children);
-// inputs
-const type = document.querySelector('#type');
-const tofrom = document.querySelector('#tofrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
-// list template instance
-const ul = document.querySelector('ul');
-const list = new ListTemplate(ul);
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let doc;
-    if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
-    }
-    else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
-    }
-    list.render(doc, type.value, 'end');
-});
-// ENUMS
-var ResourceType;
-(function (ResourceType) {
-    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
-    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
-    ResourceType[ResourceType["FILM"] = 2] = "FILM";
-    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
-})(ResourceType || (ResourceType = {}));
-;
-const docOne = {
-    uid: 1,
-    resourceType: ResourceType.BOOK,
-    data: { title: 'name of the wind' }
-};
-const docTwo = {
-    uid: 10,
-    resourceType: ResourceType.DIRECTOR,
-    data: { title: 'name of the wind' }
-};
-console.log(docOne);
-console.log(docTwo);
+import { HasFormatter } from './interfaces/HasFormatter.js';
 
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+// inputs
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+
+  let values: [string, string, number];
+  values = [tofrom.value, details.value, amount.valueAsNumber];
+
+  let doc: HasFormatter;
+  if (type.value === 'invoice') {
+    doc = new Invoice(...values);
+  } else {
+    doc = new Payment(...values);
+  }
+  
+  list.render(doc, type.value, 'end');
+});
+
+// TUPLES
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
+
+let tup: [string, number, boolean] = ['ryu', 25, true];
+// tup[0] = false;
+tup[0] = 'ken';
+
+let student: [string, number];
+//student = [23564, 'chun-li'];
+student = ['chun-li', 23564];
