67cb61d5ef1efd480fd17efbc2ecf2f3f9e2be58
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-06-09 15:28:19 -0500

    tuple

--- app.ts
+++ app.ts
@@ -1,55 +1,46 @@
 import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
 import { ListTemplate } from './classes/ListTemplate.js';
-import { HasFormatter } from './interfaces/HasFormatter.js';
-
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
+const form = document.querySelector('.new-item-form');
 console.log(form.children);
-
 // inputs
-const type = document.querySelector('#type') as HTMLInputElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
-
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
 // list template instance
-const ul = document.querySelector('ul')!;
+const ul = document.querySelector('ul');
 const list = new ListTemplate(ul);
-
-form.addEventListener('submit', (e: Event) => {
-  e.preventDefault();
-
-  let doc: HasFormatter;
-  if (type.value === 'invoice') {
-    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
-  } else {
-    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
-  }
-  
-  list.render(doc, type.value, 'end');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
 });
-
 // ENUMS
-
-enum ResourceType { BOOK, AUTHOR, FILM, DIRECTOR };
-
-interface Resource<T> {
-  uid: number;
-  resourceType: ResourceType;
-  data: T;
-}
-
-const docOne: Resource<object> = {
-  uid: 1,
-  resourceType: ResourceType.BOOK,
-  data: { title: 'name of the wind' }
-}
-const docTwo: Resource<object> = {
-  uid: 10,
-  resourceType: ResourceType.DIRECTOR,
-  data: { title: 'name of the wind' }
-}
-
+var ResourceType;
+(function (ResourceType) {
+    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
+    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
+    ResourceType[ResourceType["FILM"] = 2] = "FILM";
+    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
+})(ResourceType || (ResourceType = {}));
+;
+const docOne = {
+    uid: 1,
+    resourceType: ResourceType.BOOK,
+    data: { title: 'name of the wind' }
+};
+const docTwo = {
+    uid: 10,
+    resourceType: ResourceType.DIRECTOR,
+    data: { title: 'name of the wind' }
+};
 console.log(docOne);
 console.log(docTwo);
 
